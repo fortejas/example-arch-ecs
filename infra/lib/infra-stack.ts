83c1ec80fbd10af44473060cc9eb3b744d3737f4
Jason Forte <fortejas@amazon.com>
2021-03-26 15:36:40 +0200

    Updates to the CDK

--- infra/lib/infra-stack.ts
+++ infra/lib/infra-stack.ts
@@ -1,7 +1,7 @@
 import { Vpc } from '@aws-cdk/aws-ec2';
 import { Repository } from '@aws-cdk/aws-ecr';
-import { Cluster, ContainerImage, FargateService, FargateTaskDefinition, ICluster, Secret } from '@aws-cdk/aws-ecs';
-import { ApplicationLoadBalancer } from '@aws-cdk/aws-elasticloadbalancingv2';
+import { Cluster, ContainerImage, FargateService, FargateTaskDefinition, ICluster, Protocol, Secret } from '@aws-cdk/aws-ecs';
+import { ApplicationListener, ApplicationLoadBalancer, ApplicationProtocol, ListenerAction, ListenerCondition, TargetGroupBase } from '@aws-cdk/aws-elasticloadbalancingv2';
 import * as cdk from '@aws-cdk/core';
 
 export class InfraStack extends cdk.Stack {
@@ -15,16 +15,24 @@ export class InfraStack extends cdk.Stack {
       clusterName: 'SampleClusterCDK'
     })
 
-    new ApplicationLoadBalancer(this, 'ALB', { vpc })
+    const lb = new ApplicationLoadBalancer(this, 'ALB', { vpc, internetFacing: true })
 
-    this.createService('service-a', cluster)
-    this.createService('service-b', cluster)
+    const listener = lb.addListener('Listener80', {
+      port: 80,
+      defaultAction: ListenerAction.fixedResponse(404, {
+        contentType: 'text/plain', messageBody: 'Not Found'
+      })
+    })
+
+    this.createService('/service-a', 'service-a', 1, cluster, listener)
+    this.createService('/service-b', 'service-b', 2, cluster, listener)
   }
 
-  createService(name : string, cluster : ICluster) {
+  createService(path : string, name : string, priority : number, cluster : ICluster, listener : ApplicationListener) {
     const repo = new Repository(this, `ServiceRepo_${name}`, { repositoryName: `sample/${name}` })
 
     const taskDef = new FargateTaskDefinition(this, `FargateTaskDef_${name}`, { family: `${name}` })
+
     const container = taskDef.addContainer('main', {
       image: ContainerImage.fromEcrRepository(repo, 'latest'),
       environment: {
@@ -32,10 +40,21 @@ export class InfraStack extends cdk.Stack {
       },
     })
 
+    container.addPortMappings({ containerPort: 3000, protocol: Protocol.TCP })
+
     const service = new FargateService(this, `FargateService_${name}`, {
       cluster,
       serviceName: `${name}`,
       taskDefinition: taskDef
+    })
+
+    listener.addTargets(`FargateServiceTarget_${name}`, {
+      conditions: [
+        ListenerCondition.pathPatterns([path])
+      ],
+      targets: [service],
+      protocol: ApplicationProtocol.HTTP,
+      priority: priority
     })
   }
 }
